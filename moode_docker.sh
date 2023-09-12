2e7cb0836ee96114aa5443b3f8c470674f10a8e5
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-12 16:06:42 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -16,7 +16,7 @@ echo "*********************************************"
 echo ""
 podman machine init
 podman machine stop
-podman machine set --rootful=false
+podman machine set --rootful=true
 podman machine start
 echo ""
 echo ""
@@ -29,7 +29,7 @@ sleep 3
 
 
 
-# podman run --privileged --rm tonistiigi/binfmt --install all
+podman run --privileged --rm tonistiigi/binfmt --install all
 podman run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
 
 echo ""
@@ -66,7 +66,7 @@ echo "*    create container with systemd in priviledged mode and start it    *"
 echo "************************************************************************"
 echo ""
 echo ""
-podman create --name debian-moode --restart always -v "C:\moode":/mnt/NAS:rw -v "C:\moode\sys":/sys:rw --network=host navikey/raspbian-bullseye /lib/systemd/systemd
+podman create --name debian-moode --restart always -v "C:\moode":/mnt/NAS:rw -v "C:\moode\sys":/sys:rw --network=host --security-opt seccomp:unconfined navikey/raspbian-bullseye /lib/systemd/systemd
 podman container start debian-moode
 podman exec -ti debian-moode /bin/bash -c "ip addr show"
 sleep 5
