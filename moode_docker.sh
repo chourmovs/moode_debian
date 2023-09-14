ec7f3f145f9e4c5b27c42c0db5007a6f45d43cc7
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-14 22:45:08 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -17,7 +17,7 @@ echo ""
 sudo apt update -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
