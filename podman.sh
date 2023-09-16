06f521cab7336137fee8527f4e483afad0672400
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-16 23:53:06 +0200

    Update podman.sh

--- podman.sh
+++ podman.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+printf "\ec"
+echo ""
+echo "****************************************************"
+echo "*     latest podman and qemu install script        *"
+echo "*             By chourmovs                         *"
+echo "****************************************************"
+echo ""
+echo ""
+echo ""
+echo ""
+
+sudo apt update -y | sudo apt upgrade -y
+sudo apt install aptitude
+echo 'APT::Default-Release "bullseye";' | sudo tee /etc/apt/apt.conf.d/00default
+sudo cat /etc/apt/sources.list.d/sid.list
+echo 'deb https://deb.debian.org/debian sid main' > /etc/apt/sources.list.d/sid.list
+sudo aptitude -qy install podman
+sudo rm /etc/apt/sources.list.d/sid.list
