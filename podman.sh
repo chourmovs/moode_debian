e86057dd1f20ed16af7147079f6f27e9dc2771bf
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-17 00:02:06 +0200

    Update podman.sh

--- podman.sh
+++ podman.sh
@@ -11,11 +11,11 @@ echo ""
 echo ""
 echo ""
 sudo rm /etc/apt/sources.list.d/sid.list
-sudo apt update -y | sudo apt upgrade -y
+sudo apt update -y
 sudo apt install aptitude
 echo 'APT::Default-Release "bullseye";' | sudo tee /etc/apt/apt.conf.d/00default
 sudo cat /etc/apt/sources.list.d/sid.list
 echo 'deb https://deb.debian.org/debian sid main' > /etc/apt/sources.list.d/sid.list
-sudo aptitude -qy install podman
+sudo aptitude -q -y install podman
 sudo rm /etc/apt/sources.list.d/sid.list
 sudo apt update -y
