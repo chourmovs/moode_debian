11888de80147a163d98ee48f8a9a57ae7dfdf8a7
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-17 13:25:40 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -26,11 +26,11 @@ echo "*    create container with systemd in priviledged mode and start it    *"
 echo "************************************************************************"
 echo ""
 echo ""
-sudo podman volume create moode
-sudo podman run -tid --name debian-moode --network=host --security-opt seccomp:unconfined --privileged navikey/raspbian-bullseye /lib/systemd/systemd
+podman volume create moode
+podman run -tid --name debian-moode --network=host --security-opt seccomp:unconfined --privileged navikey/raspbian-bullseye /lib/systemd/systemd
 
 # sudo podman container start debian-moode
-sudo podman generate systemd --new --files -n debian-moode
+podman generate systemd --new --files -n debian-moode
 sudo cp /home/$USER/container-debian-moode.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start container-debian-moode
@@ -47,9 +47,9 @@ echo "*********************************************"
 echo ""
 sleep 2
 
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get update -y ; sleep 3 ; apt-get upgrade -y"
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get install -y curl sudo libxaw7 ssh libsndfile1 libsndfile1-dev cifs-utils"
-sudo podman exec -ti debian-moode /bin/bash -c "apt --fix-broken install -y"
+podman exec -ti debian-moode /bin/bash -c "apt-get update -y ; sleep 3 ; apt-get upgrade -y"
+podman exec -ti debian-moode /bin/bash -c "apt-get install -y curl sudo libxaw7 ssh libsndfile1 libsndfile1-dev cifs-utils"
+podman exec -ti debian-moode /bin/bash -c "apt --fix-broken install -y"
 echo ""
 echo ""
 # read -p "Press any key to continue... " -n1 -s
@@ -61,8 +61,8 @@ echo ""
 echo ""
 sleep 1
 
-sudo podman exec -ti debian-moode /bin/bash -c "sudo sed -i 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config;"
-sudo podman exec -ti debian-moode /bin/bash -c "systemctl restart sshd"
+podman exec -ti debian-moode /bin/bash -c "sudo sed -i 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config;"
+podman exec -ti debian-moode /bin/bash -c "systemctl restart sshd"
 
 
 echo ""
@@ -72,16 +72,16 @@ echo "*********************************************"
 echo ""
 sleep 1
 
-sudo podman exec -ti debian-moode /bin/bash -c "curl -1sLf  'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' | sudo -E distro=raspbian codename=bullseye arch=armv7hf bash -"
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get update -y"
-sudo podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get install udisks nginx triggerhappy samba dnsmasq -y"
-sudo podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
+podman exec -ti debian-moode /bin/bash -c "curl -1sLf  'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' | sudo -E distro=raspbian codename=bullseye arch=armv7hf bash -"
+podman exec -ti debian-moode /bin/bash -c "apt-get update -y"
+podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
+podman exec -ti debian-moode /bin/bash -c "apt-get install udisks nginx triggerhappy samba dnsmasq -y"
+podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
 echo ""
 echo ""
 #read -p "Press any key to continue... " -n1 -s
 
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get install moode-player -y --fix-missing"
+podman exec -ti debian-moode /bin/bash -c "apt-get install moode-player -y --fix-missing"
 echo ""
 echo ""
 echo ""
@@ -89,22 +89,22 @@ echo "In general this long install return error, next move will try to fix this"
 echo ""
 echo ""
 echo ""
-sudo podman exec -ti debian-moode /bin/bash -c "apt --fix-broken install -y"
+podman exec -ti debian-moode /bin/bash -c "apt --fix-broken install -y"
 sleep 1
 echo ""
 echo ""
 echo ""
-sudo podman exec -ti debian-moode /bin/bash -c "apt-get install moode-player -y --fix-missing"
+podman exec -ti debian-moode /bin/bash -c "apt-get install moode-player -y --fix-missing"
 sleep 1
 echo ""
 echo ""
 echo ""
-sudo podman exec -ti debian-moode /bin/bash -c "apt upgrade -y"
+podman exec -ti debian-moode /bin/bash -c "apt upgrade -y"
 #sleep 1
 echo ""
 echo ""
 echo ""
-sudo podman exec -ti debian-moode /bin/bash -c "exit"       
+podman exec -ti debian-moode /bin/bash -c "exit"       
 echo ""
 
 echo ""
@@ -126,9 +126,9 @@ echo "Will change moode http port to 8008 to avoid conflict with volumio front"
 echo ""
 echo ""
 sleep 1
-sudo podman exec -ti debian-moode /bin/bash -c "sudo sed -i 's/80 /8008 /g' /etc/nginx/sites-available/moode-http.conf"
-sudo podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
-sudo podman exec -ti debian-moode /bin/bash -c "systemctl restart nginx"
+podman exec -ti debian-moode /bin/bash -c "sudo sed -i 's/80 /8008 /g' /etc/nginx/sites-available/moode-http.conf"
+podman exec -ti debian-moode /bin/bash -c "systemctl start my-service@* --all"
+podman exec -ti debian-moode /bin/bash -c "systemctl restart nginx"
 
 echo ""
 echo "****************************"
