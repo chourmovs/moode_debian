940cf3bd86ffac3bf2f638492ec1be1f0cb8c7fb
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-17 18:10:04 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -53,7 +53,7 @@ echo "*********************************************"
 echo ""
 sleep 2
 
-podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "apt-get update -y ; sleep 3 ; apt-get upgrade -y"
+podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "mv /bin/sh.real /bin/sh ; apt-get update -y ; sleep 3 ; apt-get upgrade -y"
 podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "apt-get install -y curl sudo libxaw7 ssh libsndfile1 libsndfile1-dev cifs-utils"
 podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "apt --fix-broken install -y"
 echo ""
