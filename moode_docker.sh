dbd58429f0037e438374d56b9e53552b75ceacb7
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-17 17:39:41 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -33,7 +33,7 @@ buildah build -t localhost/debian-arm -f Dockerfile .
 echo ""
 echo ""
 echo "podman run"
-podman run --systemd=always -td --name=debian-arm --network=host --security-opt seccomp:unconfined --privileged --entrypoint=/usr/bin/qemu-arm-static localhost/debian-arm -execve -0 /sbin/init /sbin/init
+podman run --systemd=always -td --name=debian-arm --network=host --security-opt seccomp:unconfined --privileged --entrypoint=/usr/bin/qemu-arm-static localhost/debian-arm -execve -0 /sbin/init /sbin/init --platform=linux/amd64
 echo ""
 echo ""
 
