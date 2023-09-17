dfec95b6d08109e6408132c121fccc1803466594
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-17 10:04:47 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -27,9 +27,7 @@ echo "************************************************************************"
 echo ""
 echo ""
 sudo podman volume create moode
-sudo podman create --name debian-moode --network=host --security-opt seccomp:unconfined \
---cgroup-parent=docker.slice --cgroupns=host \
---privileged navikey/raspbian-bullseye /lib/systemd/systemd
+sudo podman run --name debian-moode --network=host --security-opt seccomp:unconfined --privileged navikey/raspbian-bullseye /lib/systemd/systemd
 
 # sudo podman container start debian-moode
 sudo podman generate systemd --new --files -n debian-moode
