d22216a9d2ff157a2b5526faf72919a603a43008
chourmovs <40670708+chourmovs@users.noreply.github.com>
2023-09-23 19:18:40 +0200

    Update moode_docker.sh

--- moode_docker.sh
+++ moode_docker.sh
@@ -97,7 +97,7 @@ echo ""
 echo ""
 sudo podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "apt-get install udisks -y"
 sudo podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "export DEBIAN_FRONTEND=noninteractive | rm -rf /var/lib/dpkg/info/udisks.postinst | yes "" | dpkg --configure -a |yes "" | unset DEBIAN_FRONTEND "
-read -p "Udisk finished Press any key to continue... " -n1 -s
+# read -p "Udisk finished Press any key to continue... " -n1 -s
 
 sudo podman exec -it debian-arm /usr/bin/qemu-arm-static -execve /bin/bash -c "apt-get install nginx triggerhappy samba dnsmasq -y"
 echo ""
